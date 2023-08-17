3625329406d0dcc8e2ebb8646defa45adf9eed6f
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2023-08-17 18:10:34 +0300

    Storing The Data Via SharedPreferences And Preloading It

--- app/src/main/java/com/example/weatherapp/Constants.kt
+++ app/src/main/java/com/example/weatherapp/Constants.kt
@@ -10,6 +10,8 @@ object Constants{
     const val APP_ID: String = "a8163c2b9f4d085c42d9ccda285a8f2a"
     const val BASE_URL: String = "https://api.openweathermap.org/data/"
     const val METRIC_UNIT: String = "metric"
+    const val PREFERENCE_NAME = "WeatherAppPreference"
+    const val WEATHER_RESPONSE_DATA = "weather_response_data"
 
 
     fun isNetworkAvailable(context: Context): Boolean {
